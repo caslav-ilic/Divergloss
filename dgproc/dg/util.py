3c866541f8fd7055971738b7cc3e024752f9f6b2
Chusslove Illich (Часлав Илић) <caslav.ilic@gmx.net>
2008-05-14 00:11:00 +0200

    Utilities for language-aware sorting.

--- dgproc/dg/util.py
+++ dgproc/dg/util.py
@@ -78,10 +78,97 @@ def np_(msgctxt, msgid, msgid_plural, n):
 
 
 # --------------------------------------
+# Language-aware sorting.
+
+# Set locale for requested language, and return reset function.
+def _set_lang_locale (lang):
+
+    if lang is None:
+        return lambda: True
+
+    # Work around some compatibility problems.
+    nlocnames = []
+    if 0: pass
+    elif lang == "sr":
+        nlocnames.append("sr_RS.UTF-8")
+        nlocnames.append("sr_CS.UTF-8")
+    elif lang == "sr@latin":
+        nlocnames.append("sr_RS.UTF-8@latin")
+        nlocnames.append("sr_CS.UTF-8@latin")
+    else:
+        nlocnames.append(locale.normalize(lang + ".UTF-8"))
+
+    # Try to set one of the locales.
+    oldloc = locale.getlocale()
+    setlocname = None
+    for nlocname in nlocnames:
+        try:
+            setlocname = locale.setlocale(locale.LC_ALL, nlocname)
+            break
+        except:
+            pass
+    if setlocname is None:
+        error(p_("error message",
+                 "cannot find a locale for language '%(lang)s', "
+                 "tried: %(locales)s")
+              % dict(lang=lang, locales=" ".join(nlocnames)))
+
+    # Return reset function.
+    return lambda: locale.setlocale(locale.LC_ALL, oldloc)
+
+
+def langsort (lst, lang=None):
+    """
+    Sort a list of words using collation of the given language.
+
+    If C{lang} is C{None}, current locale is used for collation.
+
+    @param lst: list to sort, in-place
+    @type lst: list
+    @param lang: language for the collation
+    @type lang: string of C{None}
+    """
+
+    reset_locale = _set_lang_locale(lang)
+    lst.sort(locale.strcoll)
+    reset_locale()
+
+
+def langsort_tuples (lst, index, lang=None):
+    """
+    Sort a list of tuples by comparing elements of given index,
+    using collation for given language.
+
+    If C{lang} is C{None}, current locale is used for collation.
+
+    @param lst: list to sort, in-place
+    @type lst: list of tuples
+    @param index: sort by tuple elements at this position
+    @type index: int
+    @param lang: language for the collation
+    @type lang: string of C{None}
+    """
+
+    reset_locale = _set_lang_locale(lang)
+    lst.sort(lambda x, y: locale.strcoll(x[index], y[index]))
+    reset_locale()
+
+
+# --------------------------------------
 # Miscellaneous.
 
-# Convert object into string using local encoding
 def lstr (obj):
+    """
+    Convert object into locale-encoded string.
+
+    The object has to have the C{__str__} method defined.
+
+    @param obj: an object
+    @type obj: object
+
+    @return: string representation of the object
+    @rtype: string
+    """
 
     cmdlenc = locale.getdefaultlocale()[1]
     return repr(obj).decode("unicode_escape").encode(cmdlenc)
